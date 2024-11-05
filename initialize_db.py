1dd692c9328ae73d5c398b7fe2030729d81d35d3
Madhav-M-Malhotra <madhavmalhotra107@gmail.com>
2024-11-05 20:35:41 +0530

    final commit

--- initialize_db.py
+++ initialize_db.py
@@ -41,9 +41,18 @@ def connect_to_mysql():
         if connection.is_connected():
             my_con = connection.cursor()
 
-            #DataBase : Student
-            my_con.execute("Create database if not exists student")
-            my_con.execute("Use student")
+            #DataBase : Student 1
+            my_con.execute("Create database if not exists AU4382917")
+            my_con.execute("Use AU4382917")
+            #Table : Quiz List
+            my_con.execute("Create table if not exists quiz_list(id varchar(20), result int, time_remaining int)")
+            #Table : Answer Sheet
+            #could fit a 385-500 words answer and 77-100 words feedback
+            my_con.execute("Create table if not exists answer_sheet(id varchar(5), ans varchar(2500), marks int, feedback varchar(500))")
+
+            #DataBase : Student 2
+            my_con.execute("Create database if not exists AU7625493")
+            my_con.execute("Use AU7625493")
             #Table : Quiz List
             my_con.execute("Create table if not exists quiz_list(id varchar(20), result int, time_remaining int)")
             #Table : Answer Sheet
@@ -51,14 +60,14 @@ def connect_to_mysql():
             my_con.execute("Create table if not exists answer_sheet(id varchar(5), ans varchar(2500), marks int, feedback varchar(500))")
 
             #DataBase : Teacher
-            my_con.execute("Create database if not exists teacher")
-            my_con.execute("Use teacher")
+            my_con.execute("Create database if not exists `684237915`")
+            my_con.execute("Use `684237915`")
             #Table : Quiz List
             my_con.execute("Create table if not exists quiz_list(id varchar(20))")
 
-            #DataBase : Subject
-            my_con.execute("Create database if not exists subject")
-            my_con.execute("Use subject")
+            #DataBase : Subject 1
+            my_con.execute("Create database if not exists CSC210")
+            my_con.execute("Use CSC210")
             #Table : Quiz List
             my_con.execute("Create table if not exists quiz_list(id varchar(20), status varchar(10), duration int, start time, end time, passwd varchar(127), date date, marks int, sections varchar(5))")
             #Table : Question Paper
@@ -66,8 +75,28 @@ def connect_to_mysql():
             my_con.execute("Create table if not exists question_paper(id varchar(5), que varchar(1000), que_image longblob, a varchar(300), b varchar(300), c varchar(300), d varchar(300), marks int, ans varchar(4), grading_type varchar(10))")
             #Table : Sections List
             my_con.execute("Create table if not exists sections_list(section int)")
-            #Table : Student List
-            my_con.execute("Create table if not exists student_list(id varchar(10), name varchar(50))")
+            my_con.execute("Insert ignore into sections_list values(1)")
+            #Table : Section 1 Student List
+            my_con.execute("Create table if not exists section_1(id varchar(10), name varchar(50))")
+            my_con.execute("Insert ignore into section_1 values('AU4382917', 'Liam Parker'),('AU7625493', 'Sophia Bennett')")
+
+            #DataBase : Subject 2
+            my_con.execute("Create database if not exists MAT248")
+            my_con.execute("Use MAT248")
+            #Table : Quiz List
+            my_con.execute("Create table if not exists quiz_list(id varchar(20), status varchar(10), duration int, start time, end time, passwd varchar(127), date date, marks int, sections varchar(5))")
+            #Table : Question Paper
+            #could fit a 385-500 words long question and each option could fit 46-60 words
+            my_con.execute("Create table if not exists question_paper(id varchar(5), que varchar(1000), que_image longblob, a varchar(300), b varchar(300), c varchar(300), d varchar(300), marks int, ans varchar(4), grading_type varchar(10))")
+            #Table : Sections List
+            my_con.execute("Create table if not exists sections_list(section int)")
+            my_con.execute("Insert ignore into sections_list values(1),(2)")
+            #Table : Section 1 Student List
+            my_con.execute("Create table if not exists section_1(id varchar(10), name varchar(50))")
+            my_con.execute("Insert ignore into section_1 values('AU4382917', 'Liam Parker')")
+            #Table : Section 2 Student List
+            my_con.execute("Create table if not exists section_2(id varchar(10), name varchar(50))")
+            my_con.execute("Insert ignore into section_2 values('AU7625493', 'Sophia Bennett')")
 
             #DataBase : Retest
             my_con.execute("Create database if not exists retest")
