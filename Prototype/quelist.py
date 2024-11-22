b103f8bf6019695a62bf888c1aec447caaf9e659
Madhav-M-Malhotra <madhavmalhotra107@gmail.com>
2024-11-22 19:41:15 +0530

    quelist prototype

--- Prototype/quelist.py
+++ Prototype/quelist.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self, que : int):
+    def __init__(self, que):
         self.que = que
         self.next = None
         self.prev = None
@@ -10,29 +10,11 @@ class QueList:
         self.head = None
         self.tail = None
 
-    def add(self, que : int):
-        temp = Node(que)
-        temp.next = self.head
-        if(self.head):
-            self.head.prev = temp
-        self.head = temp
-    
-    def delete(self, key : int):
-        temp = self.head
-
-        if temp and temp.que == key:
-            self.head = temp.next
-            temp = None
-            return
-        
-        while(temp.next):
-            temp = temp.next
-
-            if temp.que == key:
-                temp.prev.next = temp.next
-                if(temp.next):
-                    temp.next.prev = temp.prev
-                temp = None
-                return
-            
-        print(key,"doesn't exisit")
+    def add(self, que):
+        if self.head:
+            self.tail.next = Node(que)
+            self.tail.next.prev = self.tail
+            self.tail = self.tail.next
+        else:
+            self.head = Node(que)
+            self.tail = self.head
